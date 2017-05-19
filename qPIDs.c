e01f6ee389f7f6e3a74c61bb91278d54031950d5
kharsa <alan@satellogic.com>
2017-05-19 08:55:01 -0300

    Bugfix?

--- qPIDs.c
+++ qPIDs.c
@@ -1,133 +1,146 @@
-/* 
+/*
  * The MIT License (MIT)
  * Copyright (c) <2012> <Alan Kharsansky>
 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights 
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is furnished 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
  * to do so, subject to the following conditions:
 
- * The above copyright notice and this permission notice shall be included in all 
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
- * THE SOFTWARE. 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
 */
 
-#include "qPIDs.h"
 #include <stdio.h>
 #include <math.h>
+#include <platform/lib/qPIDs.h>
 
-void qPID_Init(qPID * q){
-	q->ctx.Ui_old = 0.0;
-	q->ctx.Ud_old = 0.0;
-	q->ctx.PV_old = 0.0;
-	q->ctx.SP_old = 0.0;
+void qPID_Init(qPID *q)
+{
+    q->ctx.Ui_old = 0.0;
+    q->ctx.Ud_old = 0.0;
+    q->ctx.PV_old = 0.0;
+    q->ctx.SP_old = 0.0;
+    q->Mode = OFF;
 }
 
-float qPID_Process_(qPID * q, float Input, float PV, float terms[]){
+float qPID_Process_(qPID *q, float Input, float PV, float terms[])
+{
 
 
-	// =====================================
-	//   MANUAL
-	// =====================================
-	// Input = manual input -> CO
+    // =====================================
+    //   MANUAL
+    // =====================================
+    // Input = manual input -> CO
 
-	// =====================================
-	//   AUTOMATIC
-	// =====================================
-	// Input = Setpoint
+    // =====================================
+    //   AUTOMATIC
+    // =====================================
+    // Input = Setpoint
 
-	// For local use
-	float ControllerOutput;
-	float Up, Ui, Ud;
-	float Kp, Ki, Kd_a, Kd_b,Kd_c;
-
-
-	if (fabs(q->Ti)<EPSILON){
-		q->Ti = EPSILON;
-	}
-	Kp = q->K;
-	Ki = ((q->K) * (q->Ts) )/ (q->Ti);
-	Kd_a = q->Td/(q->Td + q->Nd*q->Ts) ;
-	Kd_b = (q->K*q->Td*q->Nd)/(q->Td + q->Nd*q->Ts);
-	Kd_c = (q->c*q->K*q->Td*q->Nd)/(q->Td + q->Nd*q->Ts);
+    // For local use
+    float ControllerOutput;
+    float Up, Ui, Ud;
+    float Kp, Ki, Kd_a, Kd_b, Kd_c;
 
 
-	// Proportional gain
-	Up = Kp*( ( (q->b)*(Input) ) - PV);
+    if (fabsf(q->Ti) < EPSILON) {
+        q->Ti = EPSILON;
+    }
+    if (fabsf(q->Nd) < EPSILON) {
+        q->Nd = EPSILON;
+    }
+    Kp = q->K;
+    Ki = ((q->K) * (q->Ts)) / (q->Ti);
+    Kd_a = q->Td / (q->Td + q->Nd * q->Ts) ;
+    Kd_b = (q->K * q->Td * q->Nd) / (q->Td + q->Nd * q->Ts);
+    Kd_c = (q->c * q->K * q->Td * q->Nd) / (q->Td + q->Nd * q->Ts);
 
-	// Deriative gain with filter
-	Ud = Kd_a * (q->ctx.Ud_old) - Kd_b*(PV-q->ctx.PV_old) + Kd_c*(Input-q->ctx.SP_old);
 
-	// Get last integral
-	Ui =  q->ctx.Ui_old;
+    // Proportional gain
+    Up = Kp * (((q->b) * (Input)) - PV);
 
-	// Calculate controler output for Automatic or manual mode
+    // Deriative gain with filter
+    Ud = Kd_a * (q->ctx.Ud_old) - Kd_b * (PV - q->ctx.PV_old) + Kd_c * (Input - q->ctx.SP_old);
 
-	switch (q->Mode){
-		case MANUAL:
-			ControllerOutput = Input;
+    // Get last integral
+    Ui =  q->ctx.Ui_old;
 
-			if (q->Bumpless == ENABLED){
-				q->ctx.Ui_old = PV;
-			}
+    // Calculate controler output for Automatic or manual mode
 
-			break;
+    switch (q->Mode) {
+        case MANUAL:
+            ControllerOutput = Input;
 
-		case AUTOMATIC:
-			ControllerOutput =  Up + Ui + Ud;
-			if (ControllerOutput > q->OutputMax){
-				ControllerOutput = q->OutputMax;
-			}else if (ControllerOutput < q->OutputMin){
-				ControllerOutput = q->OutputMin;
-			}
-			break;
+            if (q->Bumpless == ENABLED) {
+                q->ctx.Ui_old = PV;
+            }
 
-		case RELAY:
-			if ((Input-PV)>=0){
-				ControllerOutput = q->OutputMax;
-			}else{
-				ControllerOutput = q->OutputMin;
-			}
-			break;
-		default:
-			// ERROR
-			ControllerOutput = NAN;
-			break;
-	}
+            break;
 
-	// Output parameters for debug
-	if (terms!=NULL){
-		terms[0] = Up;
-		terms[1] = Ui;
-		terms[2] = Ud;
-	}
+        case AUTOMATIC:
+            ControllerOutput =  Up + Ui + Ud;
+            if (ControllerOutput > q->OutputMax) {
+                ControllerOutput = q->OutputMax;
+            } else if (ControllerOutput < q->OutputMin) {
+                ControllerOutput = q->OutputMin;
+            }
+            break;
 
-	// Calc de integral for the next step
-	Ui = q->ctx.Ui_old + Ki*((Input)-PV);
+        case RELAY:
+            if ((Input - PV) >= 0) {
+                ControllerOutput = q->OutputMax;
+            } else {
+                ControllerOutput = q->OutputMin;
+            }
+            break;
+        case OFF:
+            ControllerOutput = 0;
+            break;
 
-	// Anti Windup
-	if ( (q->AntiWindup == ENABLED) && (q->Mode==AUTOMATIC) ){
-		if (Ui>=q->OutputMax){
-			Ui = q->OutputMax;
-		}else if (Ui<=q->OutputMin){
-			Ui = q->OutputMin;
-		}
-	}
+        default:
+            // ERROR
+            ControllerOutput = NAN;
+            break;
+    }
 
-	// Save context for next step.
-	q->ctx.Ui_old = Ui;
-	q->ctx.Ud_old = Ud;
-	q->ctx.PV_old = PV;
-	q->ctx.SP_old = Input;
+    // Output parameters for debug
+    if (terms != NULL) {
+        terms[0] = Up;
+        terms[1] = Ui;
+        terms[2] = Ud;
+    }
 
-	return ControllerOutput;
+    // Anti Windup
+    if ((q->AntiWindup == ENABLED) && (q->Mode == AUTOMATIC)) {
+        //if (Ui >= q->OutputMax) {
+        if (ControllerOutput >= q->OutputMax) {
+            // do not integrate anymore
+        } else if (ControllerOutput <= q->OutputMin) {
+            // do not integrate anymore
+        } else {
+            Ui = q->ctx.Ui_old + Ki * ((Input) - PV);
+        }
+    } else {
+        // Calc de integral for the next step in any other case
+        Ui = q->ctx.Ui_old + Ki * ((Input) - PV);
+    }
+
+    // Save context for next step.
+    q->ctx.Ui_old = Ui;
+    q->ctx.Ud_old = Ud;
+    q->ctx.PV_old = PV;
+    q->ctx.SP_old = Input;
+
+    return ControllerOutput;
 }
--- qPIDs.h
+++ qPIDs.h
@@ -1,24 +1,24 @@
-/* 
+/*
  * The MIT License (MIT)
  * Copyright (c) <2012> <Alan Kharsansky>
 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights 
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is furnished 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
  * to do so, subject to the following conditions:
 
- * The above copyright notice and this permission notice shall be included in all 
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
- * THE SOFTWARE. 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
 */
 
 #ifndef __qPID_CONTROLLER_H__
@@ -30,56 +30,57 @@
 // Types
 //================================================================
 
-typedef enum{
-	DISABLED=0,
-	ENABLED
+typedef enum {
+    DISABLED = 0,
+    ENABLED
 } qPID_Feature;
 
-typedef enum{
-	MANUAL=0,
-	AUTOMATIC,
-	RELAY
+typedef enum {
+    MANUAL = 0,
+    AUTOMATIC,
+    RELAY,
+    OFF
 } qPID_Mode;
 
-typedef struct{
-	float PV_old;
-	float Ui_old;
-	float Ud_old;
-	float SP_old;
+typedef struct {
+    float PV_old;
+    float Ui_old;
+    float Ud_old;
+    float SP_old;
 } qPID_Context;
 
-typedef struct{
+typedef struct {
 
-	// Parameters:
-	float K, Ti, Td;	// For use in NON-INT or INT modes
+    // Parameters:
+    float K, Ti, Td;    // For use in NON-INT or INT modes
 
-	float OutputMax;	// For windup
-	float OutputMin;	// For windup
+    float OutputMax;    // For windup
+    float OutputMin;    // For windup
 
-	float Nd;			// For derivator
-	float b, c;			// For setpoint Weighting
+    float Nd;           // For derivator
+    float b, c;         // For setpoint Weighting
 
-	float Ts;			// General propoerty
-	
-	// Features:
-	qPID_Mode			Mode;
-	qPID_Feature 		AntiWindup;
-	qPID_Feature 		Bumpless;
+    float Ts;           // General propoerty
 
-	qPID_Context		ctx;
+    // Features:
+    qPID_Mode           Mode;
+    qPID_Feature        AntiWindup;
+    qPID_Feature        Bumpless;
+
+    qPID_Context        ctx;
 
 } qPID;
 
 //================================================================
 // Defines
 //================================================================
-#define	EPSILON	0.0000001
+#define EPSILON 0.0000001f
 
 //================================================================
 // Prototypes
 //================================================================
-void qPID_Init(qPID * q);
-float qPID_Process_(qPID * q, float Input, float ProcessVariable, float terms[]);
-#define qPID_Procees(pPID,input,pv) qPID_Process_(pPID,input,pv,NULL);
+void qPID_Init(qPID *q);
+float qPID_Process_(qPID *q, float Input, float ProcessVariable, float terms[]);
+#define qPID_Process(pPID,input,pv) qPID_Process_(pPID,input,pv, NULL);
 
 #endif
